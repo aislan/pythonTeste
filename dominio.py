e63ae28ccc5818fd4da2f49fb1ff41b98a0ebf24
aislan rafael <aislanmaster@gmail.com>
2019-08-04 15:15:56 -0300

    adicionando novas regras e colocando tratamento de exceção

--- dominio.py
+++ dominio.py
@@ -21,16 +21,15 @@ class Leilao:
 		self.menor_lance = sys.float_info.max
 		
 	def propoe(self,lance):
-		self.__lances.append(lance)
-		for lance in self.__lances:
-    		#print('teste')
-			if lance.valor > self.maior_lance:
-    			#print('teste')
-				self.maior_lance = lance.valor
-			if lance.valor < self.menor_lance:
-    			#print('teste')
-				self.menor_lance = lance.valor
-
+		if not self.__lances or self.__lances[-1].usuario != lance.usuario and self.__lances[-1].valor<lance.valor:
+			self.__lances.append(lance)
+			for lance in self.__lances:
+				if lance.valor > self.maior_lance:
+					self.maior_lance = lance.valor
+				if lance.valor < self.menor_lance:
+					self.menor_lance = lance.valor
+		else:
+			raise ValueError("Erro ao propor lance!")
 
 	@property
 	def lances(self):
--- test_leilao.py
+++ test_leilao.py
@@ -24,24 +24,17 @@ class TestLeilao(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    def test_deve_retornar_o_menor_e_o_maior_valor_quando_adicionados_em_ordem_decrescente(self):
+    def test_deve_retornar_erro_quando_um_lance_menor_que_o_anterior_for_proposto(self):
         
         aline = Usuario('Aline')
 
         
         lance_da_aline = Lance(aline,150.00)
 
-
-        self.leilao.propoe(lance_da_aline)
-        self.leilao.propoe(self.lance_do_aislan)
-        
-
-        menor_valor_esperado = 100.00
-        maior_valor_esperado = 150.00
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
-
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(lance_da_aline)
+            self.leilao.propoe(self.lance_do_aislan)
+            
     def test_deve_retornar_marior_e_menor_lance_igual_quando_tiver_apenas_um_lance(self):
      
         self.leilao.propoe(self.lance_do_aislan)
@@ -63,12 +56,39 @@ class TestLeilao(TestCase):
 
 
         self.leilao.propoe(self.lance_do_aislan)
-        self.leilao.propoe(lance_da_rafaela)
         self.leilao.propoe(lance_da_aline)
+        self.leilao.propoe(lance_da_rafaela)
+       
 
         menor_valor_esperado = 100.00
         maior_valor_esperado = 120.00
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+    
+    #se não houver nenhum lance deve aceitar um novo lance
+    def test_se_nao_houve_nenhum_lance_deve_aceitar_um_novo_lance(self):
+        self.leilao.propoe(self.lance_do_aislan)
+        quantidade_de_lances = len(self.leilao.lances)
+        self.assertEqual(1,quantidade_de_lances)
 
+    #se já houver um lance deve aceitar um outro lance de outro usuário
+    def test_se_ja_houve_um_lance_deve_aceitar_um_outro_lance_de_outro_usuario(self):
+        aline = Usuario("Aline")
+        lance_da_aline = Lance(aline,200)
+
+        self.leilao.propoe(self.lance_do_aislan)
+        self.leilao.propoe(lance_da_aline)
+
+        quantidade_de_lances = len(self.leilao.lances)
+
+        self.assertEqual(2,quantidade_de_lances)
+
+    #se já houve um lande um usuário o próximo lance não deve ser do mesmo usuário
+    def test_se_ja_houve_um_lance_de_um_usuario_o_proximo_lance_nao_deve_ser_do_mesmo_usuario(self):
+        aislan = self.aislan
+        lance_do_aislan = Lance(aislan,300)
+
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(self.lance_do_aislan)
+            self.leilao.propoe(lance_do_aislan)
